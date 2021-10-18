de0f0ed486e034301e885c08350d14303accd63c
nsterk <nsterk@student.codam.nl>
2021-10-19 01:44:54 +0200

    deleted unused function sort_to_five

--- srcs/sort/sort_small.c
+++ srcs/sort/sort_small.c
@@ -6,7 +6,7 @@
 /*   By: naomisterk <naomisterk@student.codam.nl      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/22 15:48:08 by naomisterk    #+#    #+#                 */
-/*   Updated: 2021/10/19 01:41:45 by naomisterk    ########   odam.nl         */
+/*   Updated: 2021/10/19 01:43:42 by naomisterk    ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,16 +36,6 @@ void	sort_three(t_stacks *stacks)
 		rotate(&stacks->a, "ra", &stacks->ops, 1);
 	if (stacks->a->pos > stacks->a->next->pos)
 		swap(stacks->a, "sa", &stacks->ops);
-}
-
-void	sort_to_five(t_stacks *stacks)
-{
-
-	while (list_size(stacks->a) > 3)
-		lowest_to_b(stacks);
-	sort_three(stacks);
-	while (stacks->b)
-		push(stacks, "pa", 1);
 }
 
 void	sort_small(t_stacks *stacks)
